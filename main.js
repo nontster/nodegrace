a114a4cd316a5cee8938af970bd27b99268307b2
Nont Banditwong <nontster@gmail.com>
2022-12-30 20:59:51 +0700

    Add cluster module

--- main.js
+++ main.js
@@ -4,57 +4,74 @@ const port = 8080;
 const graceDelaySeconds = process.env.GRACE_DELAY_SECONDS || 10;
 const express = require("express");
 const actuator = require('express-actuator');
+const cluster = require("cluster");
+const totalCPUs = require("os").cpus().length;
 
+if (cluster.isMaster) {
+  console.log(`Number of CPUs is ${totalCPUs}`);
+  console.log(`Master ${process.pid} is running`);
 
-app = express();  
-app.use(actuator());
-
-app.get("/", (req, res) => {
-  res.send('ok')
-});
-
-app.get("/shutdown", async (req, res) => {
-  console.log('start counter');
-  
-  try{
-    await counter()
-  } catch(error){
-    return next(error);
+  // Fork workers.
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
   }
 
-  console.info('Simulate graceful shutdown by delay for %ds', graceDelaySeconds);
-  await waitforme(graceDelaySeconds*1000);
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+} else {
+  app = express();
+  app.use(actuator());
 
-  res.send("end shutdown");
-});
+  app.get("/", (req, res) => {
+    res.send('ok')
+  });
 
-let server = app.listen(port, () => {
-  console.log(`The Express.js server has started and is listening on port number: ${port}`);
-});
+  app.get("/shutdown", async (req, res) => {
+    console.log('start counter');
 
-const gracefulShutdown = () => {  
+    try {
+      await counter()
+    } catch (error) {
+      return next(error);
+    }
+
+    console.info('Simulate graceful shutdown by delay for %ds', graceDelaySeconds);
+    await waitforme(graceDelaySeconds * 1000);
+
+    res.send("end shutdown");
+  });
+
+  let server = app.listen(port, () => {
+    console.log(`The Express.js server has started and is listening on port number: ${port}`);
+  });
+
+  const gracefulShutdown = () => {
     console.info('Got SIGTERM. graceful shutdown start', new Date().toISOString());
     server.close(() => {
-        console.log('Closed out remaining connections.');
-        process.exit();
+      console.log('Closed out remaining connections.');
+      process.exit();
     });
- }
+  }
 
- // listen for TERM signal .e.g. kill
-process.on('SIGTERM', gracefulShutdown)
-// listen for INT signal e.g. Ctrl-C
-process.on('SIGINT', gracefulShutdown)
+  // listen for TERM signal .e.g. kill
+  process.on('SIGTERM', gracefulShutdown)
+  // listen for INT signal e.g. Ctrl-C
+  process.on('SIGINT', gracefulShutdown)
 
-async function counter() {
+  async function counter() {
     let count = 0;
     setInterval(() => {
       count++;
       console.log(count);
     }, 1000);
-}
+  }
 
-function waitforme(millisec) {
-  return new Promise(resolve => {
+  function waitforme(millisec) {
+    return new Promise(resolve => {
       setTimeout(() => { resolve('') }, millisec);
-  })
+    })
+  }
 }
