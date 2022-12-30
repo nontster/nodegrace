0e28399346adf28d4a8058147ec405f6053e2989
Nont Banditwong <nontster@gmail.com>
2022-12-30 19:48:09 +0700

    Add k6 performance test script

---
+++ k6/script.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    stages: [
+      { duration: '30s', target: 50 },
+      { duration: '30s', target: 100 },
+      { duration: '30s', target: 0 },
+    ],
+};
+
+export default function () {
+  const res = http.get('http://localhost:8080');
+  check(res, { 'status was 200': (r) => r.status == 200,
+               'verify homepage text': (r) => r.body.includes('ok'),
+        });
+  sleep(1);
+}
